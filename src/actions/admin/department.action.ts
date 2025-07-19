538f3d5e60a2879a51abc8612db050a802522af4
Ameya Badge <ameyabadge@gmail.com>
2025-07-19 15:42:16 +0530

    added getUserDepartmentId server action

---
+++ src/actions/admin/department.action.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+
+export const getCurrentUserDepartmentId = async () => {
+  try {
+    const { userId, sessionClaims } = await auth();
+    if (!userId) throw new Error("Unauthorized access!");
+
+    return sessionClaims.metadata.departmentId;
+  } catch (error) {
+    console.log("Error getting user's departmentId : ", error);
+    return;
+  }
+};
